48ce104cf0ee442164a20eaa0cb70486728bee80
sanjay-desai82 <sanjay.desai82+github@gmail.com>
2022-05-03 13:22:59 +0530

    [test Sanjay] MASTER YODA: Given a sentence, return a sentence with the words reversed

---
+++ Sanjay/Master yoda.py
@@ -0,0 +1,14 @@
+#Given a sentence, return a sentence with the words reversed
+def master_yoda(a):
+    a=str(a)
+    b=a.split()
+    c=b[::-1]
+    x=" ".join(c)
+    return(x)
+
+x=master_yoda('I am home')
+print(x)
+x=master_yoda('We are ready')
+print(x)
+x=master_yoda("Tiger are biggest cat's in the family")
+print(x)
