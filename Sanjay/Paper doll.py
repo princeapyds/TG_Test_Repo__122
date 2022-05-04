118329af20288d258f21d45718b725d27485505a
sanjay-desai82 <sanjay.desai82+github@gmail.com>
2022-05-04 09:29:54 +0530

    [test Sanjay] PAPER DOLL: Given a string, returns a string where for every character in the original there are three characters

--- Sanjay/Paper doll.py
+++ Sanjay/Paper doll.py
@@ -1,9 +1,13 @@
 # PAPER DOLL: Given a string, return a string where for every character in the original there are three characters
 
-b=" "
-c=" "
-for i in range(len(a)):
-    #print(i)
-    b=a[i]*3
-    c = c+b
-print(c)
+def paper_doll(a):
+    a=str(a)
+    b, c=" ", " "
+    for i in range(len(a)):
+        b=a[i]*3
+        c = c+b
+    return c
+
+
+print(paper_doll('Hello'))
+print(paper_doll('Mississippi'))
