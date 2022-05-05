a6cf2658f71cf7fad715616e2b01b2e7a056cc69
Administrator <RahulG1991>
2022-05-05 13:32:56 +0530

    Module1

---
+++ Rahul/POC1/M01.py
@@ -0,0 +1,10 @@
+#THIS IS FOR MANUAL & GASOLINE CARS
+#changes done
+class Man_Gasoline:
+    colour=None
+    tank_cap=None
+    sun_roof=True
+
+    def details(self,*args):
+        print("You have entered below features : \n Colour :",self.colour,"\nTank Capacity : ",self.tank_cap,"\n Its Sunroof :",self.sun_roof)
+
